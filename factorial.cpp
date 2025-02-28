5c81703869e2a79a10e79036bc7280f0a76be9e2
syeddabdulrehmann <syedabdulrehman.uajk@gmail.com>
2025-02-28 03:23:00 -0800

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    long long factorial = 1;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (num < 0) 
+	{
+        cout << "Factorial of negative numbers is not defined." << endl;
+    }
+	else 
+	{
+        for (int i = 1; i <= num; i++) 
+		{
+            factorial *= i;
+        }
+        cout << "Factorial of " << num << " is: " << factorial << endl;
+    }
+    
+    return 0;
+}
