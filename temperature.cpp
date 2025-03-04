765769c534ba590ea83439eb476b683437171731
syeddabdulrehmann <syedabdulrehman.uajk@gmail.com>
2025-03-04 08:30:46 -0800

    Add files via upload

--- temperature.cpp
+++ temperature.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() 
+int main()
 {
-    float temperature;
-    cout << " Enter the temperature in °C: ";
-    cin >> temperature;
-    if (temperature >= 40) 
+	int temp;
+	cout << "Enter temp"<<endl;
+	cin >> temp;
+	if(temp>=40)
 	{
-        cout << "It's extremely hot. Stay indoors to avoid health risks." << endl;
-    }
-	 else if (temperature >= 30 && temperature <= 39) 
+		cout << "Stay indoor because it is extremely outside"<<endl;
+	}
+	else if(temp>=30 && temp<=39)
 	{
-        cout << "It's hot. Stay hydrated by drinking plenty of water." << endl;
-    }
-	 else if (temperature >= 20 && temperature <= 29) 
+		cout << "Drink plenty of water to stay hydrated in hot weather"<<endl;
+	}
+	else if(temp>=20 && temp<=29)
 	{
-        cout << "The weather is pleasant and suitable for outdoor activities." << endl;
-    } 
-	else 
+		cout << "The weather is pleasant and suitable for outdoor activities"<<endl;
+	}
+	else
 	{
-        cout << "It's cool. Wear warm clothes to stay comfortable." << endl;
-    }
-    return 0;
+		cout << "Wear warm clothes to stay comfortable in the outdoor weather"<<endl;
+	}
+	return 0;
 }
